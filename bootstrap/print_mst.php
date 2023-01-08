f93a3031e17a62024a919c24de6e165120363a97
David.Dewes <dade00003@stud.uni-saarland.de>
2023-01-08 13:38:20 +0100

    simpl bootstraps/print_mst

--- bootstrap/print_mst.php
+++ bootstrap/print_mst.php
@@ -1,12 +1,11 @@
 <?php
 
-use App\Http\Request;
-use App\Http\Response;
+use App\Core\DynamicLoader;
 use App\Quicky;
 
 $app = Quicky::create();
 Quicky::session()->start();
 
 Quicky::route("GET", "/", function () {
-    \App\Core\DynamicLoader::getLoader()->getMethods()->dump();
+    DynamicLoader::getLoader()->getMethods()->dump();
 });
