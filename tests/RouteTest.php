d5ad4ed6f185334472938db6a9640d411889227c
David.Dewes <dade00003@stud.uni-saarland.de>
2023-02-09 18:52:05 +0100

    continued work

--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -1,8 +1,16 @@
 <?php declare(strict_types=1);
 
+use App\Http\Route;
+
 require __DIR__ . "/QuickyTestCase.php";
 
 class RouteTest extends QuickyTestCase
 {
-
+    public function simpleAddRouteTest(): void
+    {
+        $route = new Route("GET", "/", function () {
+        }, []);
+        $this->assertInstanceOf(Route::class, $route);
+        // $this->assertTrue($route->match("/", ));
+    }
 }
--- tests/SimpleTest.php
+++ tests/SimpleTest.php
@@ -1,8 +1,8 @@
 <?php declare(strict_types=1);
 
-use App\Quicky;
-
 require __DIR__ . "/QuickyTestCase.php";
+
+use App\Quicky;
 
 final class SimpleTest extends QuickyTestCase
 {
