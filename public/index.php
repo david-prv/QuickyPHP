5b7ebf9485a0b7910720f09fe15f1be6a0cff27f
David.Dewes <dade00003@stud.uni-saarland.de>
2023-02-15 18:56:14 +0100

    Updated index explanations (they were a lot out-dated)

--- public/index.php
+++ public/index.php
@@ -13,13 +13,14 @@ require __DIR__ . "/../vendor/autoload.php";
 
 /*
 |--------------------------------------------------------------------------
-| Create QuickyPHP Application
+| Boot from Bootstrap
 |--------------------------------------------------------------------------
 |
-| Create an singleton instance of QuickyPHP. We will use
-| App with an active and secured session. Secure means, that the
-| SessionID will be regenerating over-and-over again, to protect
-| your application against basic Session-Hijacking.
+| A bootstrap is responsible for setting up the whole application:
+| It creates an singleton instance of QuickyPHP and adds some routes.
+| We will, by default, use Quicky with an active and secured session.
+| Secure means, that the SessionID will be regenerating over-and-over again,
+| to protect your application against basic Session-Hijacking.
 |
 */
 
@@ -30,17 +31,7 @@ require_once __DIR__ . "/../bootstrap/default.php";
 | Run Your Application
 |--------------------------------------------------------------------------
 |
-| To make your application working fine, you need
-| to register routes. Routes can contain RegEx patterns, named
-| variables and wildcards. Here, we just add the index route for
-| your application, such that https://your-domain.tld/ maps to your
-| defined callback behaviour.
-|
-| To do so, we import a pre-defined default bootstrap.
-| Feel free to discover all available bootstraps in /bootstrap.
-| I am also always happy to see new proposals for new ones.
-|
-| The last line then ignites the Core of QuickyPHP, where requests
+| This last line now ignites the Core of QuickyPHP, where requests
 | are going to be routed and processed, middleware going to be
 | executed and the application will work.
 |
