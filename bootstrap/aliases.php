5c188b65451f1d310a2b092fd9514b6420976bbe
david-prv <david_dewes@web.de>
2023-02-19 20:24:33 +0100

    uniform view layouts, tested another use-case in aliases.php

--- bootstrap/aliases.php
+++ bootstrap/aliases.php
@@ -11,13 +11,25 @@ App::alias("get", function (string $pattern, callable $callback, ...$middleware)
     $router->route("GET", $pattern, $callback, ...$middleware);
 });
 
+App::alias("post", function (string $pattern, callable $callback, ...$middleware) {
+    $router = App::router();
+    $router->route("POST", $pattern, $callback, ...$middleware);
+});
+
 App::get("/", function (Request $_, Response $response) {
     $response->write("<h1>Hello World</h1>");
     return $response;
 });
 
+App::post("/", function (Request $_, Response $response) {
+    $response->write("OK");
+    return $response;
+});
+
+App::router()->dump();
+
 App::alias("render", function (string $message) {
     echo $message;
 });
 
-App::render("error");
+App::render("* beep * This is a test... * boop *");
